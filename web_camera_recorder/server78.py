aa8e79f21730e008deecb049ba12f7a440727b0c
StefanPushkov <stefan.pushkov@icloud.com>
2019-10-25 18:08:20 +0300

    Add server78.py

---
+++ web_camera_recorder/server78.py
@@ -0,0 +1,19 @@
+from camera import VideoCamera
+
+def video_stream():
+    global video_camera
+    global global_frame
+
+    if video_camera == None:
+        video_camera = VideoCamera()
+
+    while True:
+        frame = video_camera.get_frame()
+
+        if frame != None:
+            global_frame = frame
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
