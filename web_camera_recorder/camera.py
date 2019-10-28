2805d12a161458a2ad2439be78acaa3bfea884c4
StefanPushkov <stefan.pushkov@icloud.com>
2019-10-28 14:34:24 +0300

    change rtsp address

--- web_camera_recorder/camera.py
+++ web_camera_recorder/camera.py
@@ -38,7 +38,7 @@ class VideoCamera(object):
 
     def __init__(self):
         # Open a camera
-        self.cap = cv2.VideoCapture('rtsp://192.168.10.165:554') #'rtsp://192.168.10.165:554'   rtsp://80.254.24.22:554
+        self.cap = cv2.VideoCapture('rtsp://80.254.24.22:554') #'rtsp://192.168.10.165:554'   rtsp://80.254.24.22:554
       
         # Initialize video recording environment
         self.is_record = False
@@ -47,7 +47,7 @@ class VideoCamera(object):
 
         # Thread for recording
         self.recordingThread = None
-        
+
     def __del__(self):
         self.cap.release()
     
