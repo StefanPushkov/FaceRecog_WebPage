bfb6f79f12630fb5aa2a5ff39b40695ba372436c
StefanPushkov <stefan.pushkov@icloud.com>
2019-10-28 14:44:01 +0300

    Try to render video in oculars

--- web_camera_recorder/server78.py
+++ web_camera_recorder/server78.py
@@ -14,9 +14,9 @@ def video_stream():
             global_frame = frame
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        else:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+        # else:
+        #    yield (b'--frame\r\n'
+        #           b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
 
 while True:
     video_stream()
