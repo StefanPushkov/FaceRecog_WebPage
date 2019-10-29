134516d4a149fa79bb7dd73788ff87a16cfb0eb9
StefanPushkov <stefan.pushkov@icloud.com>
2019-10-29 15:56:37 +0300

    Add new server.py - returns faw frames

---
+++ web_camera_recorder/frames.py
@@ -0,0 +1,21 @@
+from camera import VideoCamera
+import sys
+
+def video_stream():
+    global video_camera
+    global global_frame
+
+    if video_camera == None:
+        video_camera = VideoCamera()
+
+    while True:
+        frame = video_camera.get_frame()
+
+        if frame != None:
+            sys.stdout.write(frame.tostring())
+        # else:
+        #    yield (b'--frame\r\n'
+        #           b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+
+while True:
+    video_stream()
