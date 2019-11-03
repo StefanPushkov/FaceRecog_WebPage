488795e4844ec61c203f5b0e2a7a4345d065a042
StefanPushkov <stefan.pushkov@icloud.com>
2019-11-04 00:26:40 +0300

    forTEST

--- web_camera_recorder/forTest.py
+++ web_camera_recorder/forTest.py
@@ -10,19 +10,22 @@ import numpy as np
 import imutils
 
 # ffmpeg setup
+
+video = cv2.VideoCapture('rtsp://80.254.24.22:554')
+r, cap = video.read()
+print(str(cap.get(cv2.CAP_PROP_FPS)))
+
 p = Popen(['ffmpeg', '-f', 'rawvideo', '-pix_fmt', 'yuv420p', '-s', '1440x810', '-r', '25',
            '-i', 'pipe:0', '-c:v', 'libx264', '-crf', '20', '-preset', 'veryfast', '-f', 'flv',
            'rtmp://78.46.97.176:1935/vasrc/faceTestInput'], stdin=PIPE)
-
-video = cv2.VideoCapture('rtsp://80.254.24.22:554')
-
 while True:
     ret, frame = video.read()
     if ret:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV_I420)
-        im = Image.fromarray(frame)
-        im.save(p.stdin, 'YUV420')
+        p.stdin.write(frame.tostring())
+        # im = Image.fromarray(frame)
+        # im.save(p.stdin, 'YUV420')
     else:
         break
 
