b60b3cf9ed6f52ea33fb8291755a0a11a71eb560
StefanPushkov <stefan.pushkov@icloud.com>
2019-11-03 23:47:06 +0300

    forTEST

---
+++ web_camera_recorder/forTest.py
@@ -0,0 +1,31 @@
+
+# import packages
+from PIL import Image
+from subprocess import Popen, PIPE
+from imutils.video import VideoStream
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import cv2
+import numpy as np
+import imutils
+
+# ffmpeg setup
+p = Popen(['ffmpeg', '-f', 'rawvideo', '-pix_fmt', 'yuv420p', '-s', '1440x810', '-r', '25',
+           '-i', 'pipe:0', '-c:v', 'libx264', '-crf', '20', '-preset', 'veryfast', '-f', 'flv',
+           'rtmp://78.46.97.176:1935/vasrc/faceTestInput'], stdin=PIPE)
+
+video = cv2.VideoCapture('rtsp://80.254.24.22:554')
+
+while True:
+    ret, frame = video.read()
+    if ret:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        im = Image.fromarray(frame)
+        im.save(p.stdin, 'JPEG')
+    else:
+        break
+
+p.stdin.close()
+p.wait()
+video.release()
+cv2.destroyAllWindows()
