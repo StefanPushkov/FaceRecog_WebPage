2f2be539bff93d17042947234b38a6eff8cded8a
StefanPushkov <stefan.pushkov@icloud.com>
2019-11-03 23:19:00 +0300

    Frames Optimized

--- web_camera_recorder/framesOptimized.py
+++ web_camera_recorder/framesOptimized.py
@@ -102,7 +102,7 @@ def video_stream():
             # else:
             #    yield (b'--frame\r\n'
             #           b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
-if __name__ == '__main__':
+while True:
     video_stream()
 
 '''
