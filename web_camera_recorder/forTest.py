88b756524f84713547ef4d9a1bfb535c827a0fbf
StefanPushkov <stefan.pushkov@icloud.com>
2019-11-07 19:22:29 +0300

    trying to find optimal time to convert color

--- web_camera_recorder/forTest.py
+++ web_camera_recorder/forTest.py
@@ -24,8 +24,7 @@ def StreamRecog():
     #           'rtmp://78.46.97.176:1935/vasrc/faceTestInput']
 
     # Resized  1440x810, # Not resized 1920x1080
-    p = Popen(['ffmpeg', '-f', 'rawvideo', '-pix_fmt', 'yuv420p', '-s', '960x540',
-               '-filter_complex', '[0:v]setpts=0.5*PTS',
+    p = Popen(['ffmpeg', '-f', 'rawvideo', '-pix_fmt', 'yuv420p', '-s', '960x540', 
                '-i', '-', '-c:v', 'libx264', '-crf', '25', '-preset', 'veryfast', '-fflags',
                'nobuffer', '-f', 'flv',
                'rtmp://78.46.97.176:1935/vasrc/ttyy'], stdin=PIPE)
